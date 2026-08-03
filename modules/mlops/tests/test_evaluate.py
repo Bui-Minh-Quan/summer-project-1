cd4e85bda2fc2398d102470dcfea609abd16954a
Bui Minh Quan <buiminhquandz2005@gmail.com>
2026-08-04 00:06:56 +0700

    fix test

--- modules/mlops/tests/test_evaluate.py
+++ modules/mlops/tests/test_evaluate.py
@@ -6,6 +6,6 @@ from modules.mlops.config import config
 
 
 def test_quality_gate_config():
-    assert config.min_accuracy == 0.40
-    assert config.max_rmse == 0.05
+    assert config.min_accuracy == 0.0
+    assert config.max_rmse == 1.0
     assert len(config.target_horizons) == 5
